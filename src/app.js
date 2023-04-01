210d2a2c9a4e2eb64507aad88f1515cfd73e8d9b
Nupur Gohite <118847362+gohitenupur@users.noreply.github.com>
2023-04-01 14:32:21 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -1,22 +1,31 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     const chunks = [];
 
-    req.on('data', chunk => {
+    req.on("data", (chunk) => {
       const buf = Buffer.from(chunk);
       const str = buf.toString();
       chunks.push(str);
-      const obj = JSON.parse(chunks)
+      const obj = JSON.parse(chunks);
       const value1 = obj.num1;
       const value2 = obj.num2;
 
       // Write code here to calculate power of a number
-      
+      if (isNaN(value1) || value1 <= 0) {
+        return res.status(404).send("num1 must be a positive integer");
+      }
+
+      if (isNaN(value2) || value2 < 0) {
+        return res.status(400).send("num2 must be a non-negative integer");
+      }
+
+      const result = Math.pow(value1, value2);
+
+      res.status(200).send(`The result is ${result}`);
     });
-    }
+  }
 });
 
 module.exports = server;
-      
