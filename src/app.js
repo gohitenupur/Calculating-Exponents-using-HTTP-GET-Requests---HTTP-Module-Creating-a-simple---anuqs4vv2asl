2d16ec171c9cba2a9064a1097b46130dfed5a1f7
Nupur Gohite <118847362+gohitenupur@users.noreply.github.com>
2023-04-06 13:07:01 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -1,33 +1,29 @@
-const http = require("http");
+const http = require('http');
 
 const server = http.createServer((req, res) => {
-  if (req.method === "POST") {
+  if (req.method === 'POST') {
     const chunks = [];
 
-    req.on("data", (chunk) => {
+    req.on('data', chunk => {
       const buf = Buffer.from(chunk);
       const str = buf.toString();
       chunks.push(str);
-      const obj = JSON.parse(chunks);
+      const obj = JSON.parse(chunks)
       const value1 = obj.num1;
       const value2 = obj.num2;
 
       // Write code here to calculate power of a number
-      if (isNaN(value1) || value1 <= 0) {
-        res.writeHead(404, { "Content-Type": "text/plain" });
-        res.end(`${value1} must be a positive integer`);
+      if(isNaN(value1) || value1<=0 || isNaN(value2) || value2<0){
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('The operation cannot be performed')
       }
-
-      if (isNaN(value2) || value2 < 0) {
-        res.writeHead(400, { "Content-Type": "text/plain" });
-        res.end(`${value2} must be a non-negative integer`);
+      else{
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end(`The result is ${value1**value2}`)
       }
-
-      const result = Math.pow(value1, value2);
-      res.writeHead(404, { "Content-Type": "text/plain" });
-      res.end(`The result is ${result}`);
+      
     });
-  }
+    }
 });
 
 module.exports = server;
